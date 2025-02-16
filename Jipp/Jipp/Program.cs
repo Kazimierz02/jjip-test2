f1b3095275fe8d9fe1b9992c1ebb7ec693011c8c
Kazimierz Kostowski <kostowskikazimierz@gmail.com>
2025-02-16 11:20:51 +0100

    init

--- Jipp/Jipp/Program.cs
+++ Jipp/Jipp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Podaj swoje imię:");
+			var name = Console.ReadLine();
+
+			Console.WriteLine("Hello " + name);
+
+			int result = 5 + 9;
+			Console.WriteLine("Wynik działania 5 + 9 to: " + result);
+		}
+	}
+}
